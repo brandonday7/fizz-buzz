90eac8bc58f3504519a1a3b48503bdb5739cdd79
Brandon Day <brandontday7@gmail.com>
2019-04-08 15:07:29 -0400

    got fizzes and buzzes displaying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { fizzOrBuzz } from "./utils.js"
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      counter: 98
+      counter: 1
     }
   }
 
@@ -30,6 +32,7 @@ class App extends Component {
         <p onClick={this.decrement}>Decrement</p>
 
         <h1>{counter}</h1>
+        <h1>{fizzOrBuzz(counter)}</h1>
       </div>
     );
   }
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+const fizzOrBuzz = counter => {
+	if (!(counter % 3) && !(counter % 5)) return "FizzBuzz"
+	else if (!(counter % 3)) return "Fizz"
+	else if (!(counter % 5)) return "Buzz"
+	else return ""
+}
+
+export { fizzOrBuzz }
