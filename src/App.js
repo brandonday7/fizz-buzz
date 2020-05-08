d3e48074ea938238fb853dff70bc44315c340e0a
Brandon Day <brandontday7@gmail.com>
2020-05-08 11:39:09 -0400

    committing a horrible mistake! linter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { fizzOrBuzz } from "./utils.js"
 import UserInput from "./UserInput"
 
 import "./app.css"
-
+horrible mistake!!!!
 class App extends Component {
   constructor(props) {
     super(props)
