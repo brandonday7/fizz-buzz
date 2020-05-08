a28953d8ca9fb2060850ca927fd7e1b328e8278a
Brandon Day <brandontday7@gmail.com>
2020-05-08 16:16:02 -0400

    Make one change

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   }
   render() {
     // const lint = 4
-    // CHANGE this change
+    // CHANGE this change change
     const { counter } = this.state
     return (
       <div className="site-container">
