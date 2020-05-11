05cb08f4cb3c7a13c9e431dc85fa6db21aceb5a4
Brandon Day <brandontday7@gmail.com>
2020-05-11 11:25:01 -0400

    Try

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   }
   render() {
     // const lint = 4
-    // CHANGE this change change change change
+    // CHANGE this change change changeT
     const { counter } = this.state
     return (
       <div className="site-container">
