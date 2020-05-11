b2e81cda4cd1d2c63162437c810643c128c418e4
Brandon Day <brandontday7@gmail.com>
2020-05-11 13:19:12 -0400

    test

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   }
   render() {
     const { counter } = this.state
+    // test
     return (
       <div className="site-container">
         <div className="display">
