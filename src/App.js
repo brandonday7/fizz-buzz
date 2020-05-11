e50fb5d3f274d27e970247fbaf7b7c6c52a7ccf0
Brandon Day <brandontday7@gmail.com>
2020-05-11 13:22:58 -0400

    Fix warning

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
   }
   render() {
     const { counter } = this.state
-    const x = 5
     return (
       <div className="site-container">
         <div className="display">
