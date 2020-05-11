c4c4bda85e593702ba9de42d2a0ed4249ff3b073
Brandon Day <brandontday7@gmail.com>
2020-05-11 14:06:24 -0400

    Final changes

--- src/App.js
+++ src/App.js
@@ -27,8 +27,6 @@ class App extends Component {
   }
   render() {
     const { counter } = this.state
-    // change change chagen
-    const x = 5
     return (
       <div className="site-container">
         <div className="display">
