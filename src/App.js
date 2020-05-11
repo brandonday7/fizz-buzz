3ddd9673756f8bc2ad93982da241e199fe652e24
Brandon Day <brandontday7@gmail.com>
2020-05-11 13:16:50 -0400

    Still changes

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   }
   render() {
     const { counter } = this.state
+    // now
     return (
       <div className="site-container">
         <div className="display">
