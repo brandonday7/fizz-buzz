c844dfad835461e3616fcb1c31ded4c9708e4a5b
Brandon Day <brandontday7@gmail.com>
2020-05-11 11:47:35 -0400

    Change 3 files, lint 2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// gonna change this file too change
+// gonna change this file too change change
