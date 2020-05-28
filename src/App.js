3643f88bbe8fdb0f24aa65fe76f43ce1729587c1
Brandon Day <brandontday7@gmail.com>
2020-05-28 14:02:26 -0400

    try another change

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   render() {
     const { counter } = this.state
     // test this out out this too
-    // put and error in the file
+    // put and error in the file another change
     return (
       <div className="site-container">
         <div className="display">
